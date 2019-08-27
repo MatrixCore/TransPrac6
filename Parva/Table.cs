43910186d1a401fc3f6cea4d5541326c0a0097e7
foxbat123 <31068359+foxbat123@users.noreply.github.com>
2019-08-27 15:02:37 +0200

    Finished Task 3

--- Parva/Table.cs
+++ Parva/Table.cs
@@ -3,6 +3,7 @@
 
 using Library;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Parva {
 
@@ -19,17 +20,46 @@ namespace Parva {
     static List<Entry> list = new List<Entry>();
 
     public static void ClearTable() {
-    // Clears cross-reference table
-    // ...
+            list.Clear(); 
     } // Table.ClearTable
 
     public static void AddRef(string name, bool declared, int lineRef) {
-    // Enters name if not already there, and then adds another line reference (negative
-    // if at a declaration point in the original source program)
-    // ...
-    } // Table.AddRef
+            declared = false;
+            foreach (Entry item in list)
+            {
+                if (item.name == name)
+                {
+                    declared = true;
+                    if (item.refs.Contains(lineRef) != true) //check to see if the line reference has already been included
+                    {
+                        item.refs.Add(lineRef);
+                    }
+                    
+                }
+            }
+            if (declared != true)
+            {
+                Entry toAdd = new Entry(name);
+                toAdd.refs.Add( (lineRef * -1) );
+                list.Add(toAdd); 
+            }
+            
+            // Enters name if not already there, and then adds another line reference (negative
+            // if at a declaration point in the original source program)
+            // ...
+        } // Table.AddRef
 
     public static void PrintTable() {
+            foreach (Entry item in list)
+            {
+                IO.Write($"{item.name}\t\t");
+                foreach (int vary in item.refs)
+                {
+                    IO.Write($"{vary}   "); 
+                }
+                IO.WriteLine(); 
+            }
+
     // Prints out all references in the table (eliminate duplicates line numbers)
     //...
     } // Table.PrintTable
